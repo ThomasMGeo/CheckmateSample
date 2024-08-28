4c82d182539c43564acbc6586698e2202589a9a7
Thomas Martin <38228140+ThomasMGeo@users.noreply.github.com>
2024-08-28 14:19:12 -0600

    Update  __init__.py

--- src/CheckmateSample/ __init__.py
+++ src/CheckmateSample/ __init__.py
@@ -3,6 +3,6 @@ from importlib.metadata import version, PackageNotFoundError
 from .generator import * 
 
 try:
-    from ._version import version as __version__
+    from ._version import __version__
 except ImportError:
     __version__ = "unknown"
